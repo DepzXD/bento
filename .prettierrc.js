a8e2d2e916a99eb977a7b84cb37e72f615e05b30
depzXD <dk721572@gmail.com>
2022-02-27 21:24:47 +0530

    Initialize

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "arrowParens": "always",
+  "trailingComma": "all"
+};
---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Home from './screens/Home';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
